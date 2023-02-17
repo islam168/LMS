bb80b87881703854521fa6df8f6d3b9c1026ca20
Islamiddin Islamov <islamov.isl168@gmail.com>
2023-02-17 20:55:45 +0600

    moved all packages to the Backend folder and added course categories

--- lms/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-class Course(models.Model):
-    title = models.CharField(max_length=30)
-    content = models.TextField()
-
-    def __str__(self) -> str:
-        return self.title
--- lms/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path('course', CourseListView.as_view(), name='course_list'),
-    path('course/<int:pk>', DetailView.as_view(), name='course_detail'),
-
-]
