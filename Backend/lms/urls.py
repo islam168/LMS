7b6a6afaaf4dc2ef143f998166a665a1be7873b8
Islamiddin Islamov <islamov.isl168@gmail.com>
2023-02-18 13:23:31 +0600

    added a search by course title and category

--- Backend/lms/urls.py
+++ Backend/lms/urls.py
@@ -1,16 +1,14 @@
-from django.urls import path
 from .views import *
+from django.urls import path, include
+from rest_framework.routers import SimpleRouter
 
+router = SimpleRouter()
+router.register('course', CourseView, basename='course')
+router.register('categories', CategoryList, basename='category')
 urlpatterns = [
-    path('course_list', CourseListView.as_view({'get': 'list'}), name='course_list'),  # Страница со списком курсов без возможности их создавать
-    path('course', CourseView.as_view({'get': 'list',
-                                           'post': 'create'}), name='course_list'),  # Страница со списком курсов с возможности их создавать
-    path('course/<int:pk>', CourseView.as_view({
-        'get': 'retrieve',
-        'put': 'update',
-        'patch': 'partial_update',
-        'delete': 'destroy'
-    }), name='course_detail'),
+    path('', include(router.urls)),
+    path('course_list/', CourseListView.as_view({'get': 'list'}), name='course_list'),
+    path('category_detail/int:pk/', CategoryDetail.as_view(), name='category_detail'),
     path('categories/', CategoryList.as_view({'get': 'list',
                                               'post': 'create'}), name='category_list'),
 
--- Backend/lms/views.py
+++ Backend/lms/views.py
@@ -1,17 +1,24 @@
+from rest_framework import filters, viewsets
 from rest_framework.generics import RetrieveUpdateDestroyAPIView
-from rest_framework import viewsets
 from .models import Course, Category
 from .serializers import CourseSerializer, CategorySerializer, CourseListSerializer
+from django_filters.rest_framework import DjangoFilterBackend
+
+
+
+class CourseListView(viewsets.ModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseListSerializer
+    filter_backends = (filters.SearchFilter, DjangoFilterBackend)
+    search_fields = ('title', '=category__category_name')
+
 
 # View для страницы со списком курсов с возможности их создавать
 class CourseView(viewsets.ModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
 
-# View для страницы со списком курсов без возможности их создавать
-class CourseListView(viewsets.ModelViewSet):
-    queryset = Course.objects.all()
-    serializer_class = CourseListSerializer
+
 
 
 # class CourseListView(ListCreateAPIView):
