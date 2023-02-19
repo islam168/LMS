311d0f6a6690a815885f3a57808428d25c1f2ca0
Islamiddin Islamov <islamov.isl168@gmail.com>
2023-02-19 19:07:11 +0600

    added new conditions for discount dates

--- Backend/lms/serializers.py
+++ Backend/lms/serializers.py
@@ -35,14 +35,19 @@ class CourseListSerializer(serializers.ModelSerializer):
     def to_representation(self, instance):
         representation = super().to_representation(instance)
 
-        if str(instance.start_day) > str(datetime.date.today()):
-            representation['discount_confirmation'] = False
+        if (str(instance.start_day) == str(datetime.date.today()) and str(
+                instance.end_day) == str(datetime.date.today())) and representation['discount_confirmation'] is True:
+            representation['discount_confirmation'] = True
 
-        if str(instance.start_day) >= str(instance.end_day):
-            representation['discount_confirmation'] = False
+        else:
+            if str(instance.start_day) > str(datetime.date.today()):
+                representation['discount_confirmation'] = False
 
-        if str(instance.end_day) == str(datetime.date.today()):
-            representation['discount_confirmation'] = False
+            if str(instance.start_day) >= str(instance.end_day):
+                representation['discount_confirmation'] = False
+
+            if str(instance.end_day) == str(datetime.date.today()):
+                representation['discount_confirmation'] = False
 
         if representation['discount_confirmation'] is False:
             representation.pop('discount_confirmation')
