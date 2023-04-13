5209398bd2a7eaf2f80f254b5840fc1a20e381c5
Esenaliev Argen <56115080+Argen2002@users.noreply.github.com>
2023-04-13 13:16:34 +0600

    make branch

--- Backend/lms/models.py
+++ Backend/lms/models.py
@@ -1,3 +1,4 @@
+import objects as objects
 from django.db import models
 import datetime
 
@@ -24,3 +25,33 @@ class Course(models.Model):
     def __str__(self):
         return self.title
 
+
+#-------------------
+
+# class UserProfile(models.Model):
+#
+#     objects = objects.get(name='user_name')
+#     name = models.CharField(max_length=100)
+#     courses_taken = models.ManyToManyField('Course', through='CourseEnrollment')
+#
+#     def __str__(self):
+#         return self.name
+#
+#
+#
+#
+#
+# class CourseEnrollment(models.Model):
+#     user_profile = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+#     course = models.ForeignKey(Course, on_delete=models.CASCADE)
+#
+#     def __str__(self):
+#         return f'{self.user_profile.name} enrolled in {self.course.name}'
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    courses_taken = models.OneToManyField(Course, through='UserCourse')
+
+class UserCourse(models.Model):
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    date_enrolled = models.DateTimeField(auto_now_add=True)
